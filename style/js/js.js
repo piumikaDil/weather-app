d1d3766ef10d00a558ffce9102f542e5e1c31700
piumikaDil <piumikaDil@gmail.com>
2023-05-29 14:36:35 +0530

    Add dark mode for application

--- style/js/js.js
+++ style/js/js.js
@@ -1,8 +1,44 @@
 //////////////////////////////////// dark and light mode/////////////////////////////////////////////////////////////
-// $(".dark-btn").on("click", () => {
-//     $("body").css("backgroundColor", "black")
-//     $(".search-input").css("backgroundColor", "black")
+$(".dark-btn").on("click", () => {
+    $("body").css("backgroundColor", "black")
+    $(".search-input").css("backgroundColor", "hsl(217, 16%, 22%, 35%)")
+    $(".location").css("color", "white")
+    $(".location-img").css("color", "white")
+    $(".second-sec").css("color", "white")
+    $(".card-2").css({
+        "backgroundColor": "hsl(217, 16%, 22%, 35%",
+        "color": "white"
+    })
+    $(".card-3").css({
+        "backgroundColor": "hsl(217, 16%, 22%, 35%",
+        "color": "white"
+    })
+    $(".card-4").css({
+        "backgroundColor": "hsl(217, 16%, 22%, 35%",
+        "color": "white"
+    })
+    $(".capacity").css("color", "white")
+    $(".fa-fw").css("color", "white")
+    $(".div-last-days").css({ "backgroundColor": "hsl(217, 16%, 22%, 35%)"})
+    $(".noti-div").css("backgroundColor", "hsl(217, 16%, 22%, 35%)")
+    $(".bell").css("color", "white")
+    $(".map-sec").css("color", "white")
+    $(".last-day-1-capacity").css("color", "white")
+    $(".last-day-1").css("color", "white")
+    $(".search-input").css("color", "white")
 
+
+
+
+
+
+
+
+
+
+
+
+})
 
 // })
 
