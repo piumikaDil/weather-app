892efc38dcfdf95cc14e1bc08e92adafc2d21c69
piumikaDil <piumikaDil@gmail.com>
2023-06-14 23:36:46 +0530

    finalize

--- style/js/js.js
+++ style/js/js.js
@@ -23,13 +23,20 @@ $(".dark-btn").on("click", () => {
     $(".capacity").css({ "color": "white", "transition": "1s" })
     $(".fa-fw").css({ "color": "white", "transition": "1s" })
     $(".div-last-days").css({ "backgroundColor": "hsl(217, 16%, 22%, 35%)", "transition": "1s" })
-    $(".noti-div").css({ "backgroundColor": "hsl(217, 16%, 22%, 35%)", "transition": "1s" })
+    // $(".noti-div").css({ "backgroundColor": "hsl(217, 16%, 22%, 35%)", "transition": "1s" })
     $(".bell").css({ "color": "white", "transition": "1s" })
     $(".map-sec").css({ "color": "white", "transition": "1s" })
     $(".last-day-1-capacity").css({ "color": "white", "transition": "1s" })
     $(".last-day-1").css({ "color": "white", "transition": "1s" })
     $(".search-input").css({ "color": "white", "transition": "1s" })
     $(".icon-search").css({ "color": "white", "transition": "1s" })
+    $(".para").css({ "color": "white", "transition": "1s" })
+    $(".sunrise-image").attr({ "src": "style/icons/sunrice-white.png", "transition": "1s" });
+    $(".sunset-image").attr({ "src": "style/icons/sunset-white.png", "transition": "1s" });
+    $(".piumika").css({ "color": "white", "transition": "1s" })
+
+
+
 
 
 })
@@ -57,13 +64,20 @@ $(".light-btn").on("click", () => {
     $(".capacity").css("color", "black")
     $(".fa-fw").css("color", "black")
     $(".div-last-days").css({ "backgroundColor": "hsla(214, 13%, 78%, 0.35)" })
-    $(".noti-div").css("backgroundColor", "hsla(214, 13%, 78%, 0.35)")
+    // $(".noti-div").css("backgroundColor", "hsla(214, 13%, 78%, 0.35)")
     $(".bell").css("color", "black")
     $(".map-sec").css("color", "black")
     $(".last-day-1-capacity").css("color", "black")
     $(".last-day-1").css("color", "black")
     $(".search-input").css("color", "black")
     $(".icon-search").css("color", "black")
+    $(".para").css("color", "black")
+    $(".sunrise-image").attr("src", "style/images/icons8-sunrise-50.png");
+    $(".sunset-image").attr("src", "style/icons/sunset-black.png");
+    $(".piumika").css("color", "black")
+
+
+
 
 
 
@@ -72,16 +86,18 @@ $(".light-btn").on("click", () => {
 
 /////////////////////////////////////////fetching data//////////////////////////////////////////////////////////////////
 const today = new Date()
-const sevenDaysAgo = new Date();
-sevenDaysAgo.setDate(today.getDate() - 3);
+const DayAgo = new Date();
+DayAgo.setDate(today.getDate() - 3);
 const baseURL = 'http://api.weatherapi.com/v1/'
 let longitude = ""
 let latitude = ""
 let endDate = today.toISOString().split("T")[0];
-let startDate = sevenDaysAgo.toISOString().split("T")[0];
+let startDate = DayAgo.toISOString().split("T")[0];
 
 $(document).ready(function () {
     getPosition()
+    getCurrentDateAndTime()
+
 });
 
 
@@ -92,7 +108,6 @@ $(".btn").on("click", () => {
     let location1 = document.getElementById("search-input")
     getSearchLocation(location1)
     getHistoryWeathher(location1)
-    getCurrentDateAndTime()
     getCurrentWeathher(location1)
     getForecastWeathher(location1)
 
@@ -125,6 +140,9 @@ function showPosition(position) {
             $(".current-feel").text(data.current.feelslike_c + "°")
             $(".current-humidity").text(data.current.humidity + "%")
             $(".current-wind").text(data.current.wind_kph + " kp/h")
+            $(".main-img").attr("src", data.current.condition.icon);
+            // console.log(data);
+
         })
 
     fetch(`${baseURL}history.json?key=e21a55aa67274426b0183102231405&q=${latitude},${longitude}&end_dt=${endDate}&dt=${startDate}`)
@@ -136,13 +154,18 @@ function showPosition(position) {
             $(".day-one").text(data.forecast.forecastday[0].date)
             $(".day-two").text(data.forecast.forecastday[1].date)
             $(".day-three").text(data.forecast.forecastday[2].date)
+            $(".history-img-1").attr("src", data.forecast.forecastday[0].day.condition.icon);
+            $(".history-img-2").attr("src", data.forecast.forecastday[1].day.condition.icon);
+            $(".history-img-3").attr("src", data.forecast.forecastday[2].day.condition.icon);
+
+
 
         })
 
     fetch(`${baseURL}forecast.json?key=e21a55aa67274426b0183102231405&q=${latitude},${longitude}&days=4`)
         .then(Response => Response.json())
         .then(data => {
-            console.log(data.forecast.forecastday[1].day.maxwind_kph)
+            console.log(data.forecast.forecastday[0].astro.sunset)
             $(".future-wind-1").text(data.forecast.forecastday[0].day.maxwind_kph + " km/h")
             $(".future-wind-2").text(data.forecast.forecastday[1].day.maxwind_kph + " km/h")
             $(".future-wind-3").text(data.forecast.forecastday[2].day.maxwind_kph + " km/h")
@@ -155,6 +178,13 @@ function showPosition(position) {
             $(".future-day-one").text(data.forecast.forecastday[0].date)
             $(".future-day-two").text(data.forecast.forecastday[1].date)
             $(".future-day-three").text(data.forecast.forecastday[2].date)
+            $(".future-img-1").attr("src", data.forecast.forecastday[0].day.condition.icon);
+            $(".future-img-2").attr("src", data.forecast.forecastday[1].day.condition.icon);
+            $(".future-img-3").attr("src", data.forecast.forecastday[2].day.condition.icon);
+            $(".sunrise-time").text( data.forecast.forecastday[0].astro.sunrise);
+            $(".sunset-time").text(data.forecast.forecastday[0].astro.sunset);
+
+
 
         })
 }
@@ -191,6 +221,8 @@ const getCurrentWeathher = (location1) => {
             $(".current-feel").text(data.current.feelslike_c + "°")
             $(".current-humidity").text(data.current.humidity + "%")
             $(".current-wind").text(data.current.wind_kph + " km/h")
+            $(".main-img").attr("src", data.current.condition.icon);
+
         })
 }
 
@@ -204,6 +236,9 @@ const getHistoryWeathher = (location1) => {
             $(".day-one").text(data.forecast.forecastday[0].date)
             $(".day-two").text(data.forecast.forecastday[1].date)
             $(".day-three").text(data.forecast.forecastday[2].date)
+            $(".history-img-1").attr("src", data.forecast.forecastday[0].day.condition.icon);
+            $(".history-img-2").attr("src", data.forecast.forecastday[1].day.condition.icon);
+            $(".history-img-3").attr("src", data.forecast.forecastday[2].day.condition.icon);
 
 
           
@@ -214,7 +249,7 @@ const getForecastWeathher = (location1) => {
     fetch(`${baseURL}forecast.json?key=e21a55aa67274426b0183102231405&q=${location1.value}&days=4`)
         .then(Response => Response.json())
         .then(data => {
-            console.log(data.forecast.forecastday[1].day.maxwind_kph)
+            // console.log(data)
             $(".future-wind-1").text(data.forecast.forecastday[0].day.maxwind_kph + " km/h")
             $(".future-wind-2").text(data.forecast.forecastday[1].day.maxwind_kph + " km/h")
             $(".future-wind-3").text(data.forecast.forecastday[2].day.maxwind_kph + " km/h")
@@ -227,6 +262,12 @@ const getForecastWeathher = (location1) => {
             $(".future-day-one").text(data.forecast.forecastday[0].date)
             $(".future-day-two").text(data.forecast.forecastday[1].date)
             $(".future-day-three").text(data.forecast.forecastday[2].date)
+            $(".future-img-1").attr("src", data.forecast.forecastday[0].day.condition.icon);
+            $(".future-img-2").attr("src", data.forecast.forecastday[1].day.condition.icon);
+            $(".future-img-3").attr("src", data.forecast.forecastday[2].day.condition.icon);
+            $(".sunrise-time").text(data.forecast.forecastday[0].astro.sunrise);
+            $(".sunset-time").text(data.forecast.forecastday[0].astro.sunset);
+
 
         }) 
 
